07097b762dbba133fa83c3de49a48855857e687f
bobaejeon <bbj5608@gmail.com>
2021-11-11 14:02:20 +0900

    application화면 수정 및 treeinfo 작업중

---
+++ app/src/main/java/com/foo/garosero/data/TreeListData.java
@@ -0,0 +1,9 @@
+package com.foo.garosero.data;
+
+public class TreeListData {
+    public String gu_nm;
+    public String lc;
+    public String wdpt_nm;
+    public String width_nm;
+
+}
---
+++ app/src/main/java/com/foo/garosero/data/UserTreesData.java
@@ -0,0 +1,54 @@
+package com.foo.garosero.data;
+
+public class UserTreesData {
+    public String district;
+    public String kind;
+    public String location;
+    public String road;
+    public String start_date;
+
+    public UserTreesData(){}
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getRoad() {
+        return road;
+    }
+
+    public void setRoad(String road) {
+        this.road = road;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+
+}
