69a63c69b0c04d73f1783bfffbf2e47978a25bf7
bobaejeon <bbj5608@gmail.com>
2021-12-02 20:19:17 +0900

    refactor / level 로직 수정

--- app/src/main/java/com/foo/garosero/ui/home/treeinfo/TreeInfoAdapter.java
+++ app/src/main/java/com/foo/garosero/ui/home/treeinfo/TreeInfoAdapter.java
@@ -51,11 +51,11 @@ public class TreeInfoAdapter extends RecyclerView.Adapter<TreeInfoAdapter.ViewHo
         TreeInfo treeInfo = ud.getTreeList().get(position);
         holder.tv_tree_name.setText(treeInfo.getTree_name());
         holder.tv_tree_day.setText(getTreeDay(treeInfo.getStart_date()));
-        holder.tv_level.setText(String.valueOf(treeInfo.getLevel()));
+        holder.tv_level.setText(String.valueOf(treeInfo.getXp()/10));
         holder.progressBar.setProgress(treeInfo.getXp()%10);
         holder.tv_desc_title.setText(getDescTitle(treeInfo.getRoad(),treeInfo.getKind()));
         holder.tv_desc_content.setText(getDescContents(treeInfo.getKind()));
-        setBackgroundImageview(holder.treeCharacter, treeInfo.getKind(), treeInfo.getLevel());
+        setBackgroundImageview(holder.treeCharacter, treeInfo.getKind(), treeInfo.getXp()/10);
 
         // 나무이름 수정 버튼
         holder.btn_submit.setOnClickListener(new View.OnClickListener() {
@@ -72,11 +72,12 @@ public class TreeInfoAdapter extends RecyclerView.Adapter<TreeInfoAdapter.ViewHo
                         if(snapshot.exists()){
                             for(DataSnapshot snap:snapshot.getChildren()){
                                 HashMap<String, Object> map = new HashMap<>();
-                                map.put("tree_name", holder.tv_tree_name.getText().toString());
+                                String newName = holder.tv_tree_name.getText().toString();
+                                map.put("tree_name", newName);
                                 snap.getRef().updateChildren(map).addOnSuccessListener(new OnSuccessListener<Void>() {
                                     @Override
                                     public void onSuccess(Void unused) {
-                                        ServerHelper.initServer();
+                                        treeInfo.setTree_name(newName);
                                         Toast.makeText(view.getContext(), "완료되었습니다.", Toast.LENGTH_LONG).show();
                                     }
                                 });
