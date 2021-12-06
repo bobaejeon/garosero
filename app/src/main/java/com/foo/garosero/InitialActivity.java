0c8776bda4050baa0b4740ea95f3a5fdde48c20a
bobaejeon <bbj5608@gmail.com>
2021-12-06 21:24:49 +0900

    한번에보기 버튼

--- app/src/main/java/com/foo/garosero/InitialActivity.java
+++ app/src/main/java/com/foo/garosero/InitialActivity.java
@@ -123,14 +123,8 @@ public class InitialActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onBackPressed() { //뒤로가기 했을 때
-        // 두번 클릭시 종료
-        if (System.currentTimeMillis() - lastTimeBackPressed < 2000) {
-            finishAffinity();           // 해당 어플리케이션의 루트 액티비티를 종료
-            System.runFinalization();   // 쓰레드 종료
-            System.exit(0);       // 현재의 액티비티를 종료
-            return;
-        }
-        Toast.makeText(this, "'뒤로' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
-        lastTimeBackPressed = System.currentTimeMillis();
+        Intent intent = new Intent(InitialActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
