6bf126cb9be8472bdeaffdadfd2b3ff4c68c5221
bobaejeon <bbj5608@gmail.com>
2022-01-20 21:03:09 +0900

    1.merged web-change/ 2.altered diaries

--- app/src/main/java/com/foo/garosero/myUtil/DiaryHelper.java
+++ app/src/main/java/com/foo/garosero/myUtil/DiaryHelper.java
@@ -1,20 +1,31 @@
 package com.foo.garosero.myUtil;
 
+import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
 import com.foo.garosero.data.DiaryData;
 import com.foo.garosero.data.TreeInfo;
 import com.foo.garosero.mviewmodel.DiaryViewModel;
 import com.foo.garosero.mviewmodel.HomeViewModel;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -26,7 +37,6 @@ import java.util.Set;
 
 public class DiaryHelper {
     private final String TAG = "DiaryHelper";
-
     public void readDiaryFromFireBase(){
         String uid = FirebaseAuth.getInstance().getUid();
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
@@ -35,9 +45,15 @@ public class DiaryHelper {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 ArrayList<DiaryData> arr_diary = new ArrayList<DiaryData>();
-                for (DataSnapshot dataSnapshot : snapshot.child("Users/"+uid+"/diaries").getChildren()){
-                    DiaryData diaryData = dataSnapshot.getValue(DiaryData.class);
-                    arr_diary.add(diaryData);
+                for(DataSnapshot snap : snapshot.child("Diaries").getChildren()){
+                    try{
+                        DiaryData diary = snap.getValue(DiaryData.class);
+                        if(diary.getUid().equals(uid)){
+                            arr_diary.add(diary);
+                        }
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
                 }
                 DiaryViewModel.getDiaryDataList().setValue(arr_diary);
             }
@@ -49,10 +65,11 @@ public class DiaryHelper {
         };
         ref.addValueEventListener(postListener);
     }
+    DatabaseReference ref;
 
     public void insertDiaryToServer(DiaryData diaryData){
         if (diaryData==null) return;
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
         String uid = FirebaseAuth.getInstance().getUid();
 
         // 현재 시간을 아이디로 사용
@@ -62,37 +79,26 @@ public class DiaryHelper {
 
         // diaryData에도 ID 업데이트(BB)
         diaryData.setDiaryID(now);
+        diaryData.setUid(uid);
 
-        // diaryData를 Users>UID>diaries>now 에 업데이트
-        // 마지막 활동날짜 저장
-        ref = ref.child("Users").child(uid);
-        Map<String, Object> childUpdate = new HashMap<String, Object>();
-        childUpdate.put("/diaries/"+now, diaryData.getHash());
-        childUpdate.put("/last_activity/",now);
-        ref.updateChildren(childUpdate);
-
-        // tree xp update
-        updateTreeXp(diaryData.getTrees().keySet(), 5);
+        Map<String, DiaryData> diaries = new HashMap<String, DiaryData>();
+        diaries.put("new", diaryData);
+        uploadDiarytoServer(diaries);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void updateDiaryToServer(DiaryData oldDiary, DiaryData newDiary){
         if (oldDiary==null || newDiary==null || oldDiary.equals(newDiary)) return;
 
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
         String uid = FirebaseAuth.getInstance().getUid();
 
-        ref = ref.child("Users").child(uid).child("diaries").child(oldDiary.getDiaryID());
-        ref.updateChildren(newDiary.getHash());
+        newDiary.setUid(uid);
+        newDiary.setDiaryID(oldDiary.getDiaryID());
 
-        if (oldDiary.getTrees().equals(newDiary.getTrees())) return; // tree 값이 변경되지 않았다면 pass
-        Set<String> oldDataSet = oldDiary.getTrees().keySet();
-        Set<String> newDataSet = newDiary.getTrees().keySet();
-        oldDataSet.removeAll(newDiary.getTrees().keySet());
-        newDataSet.removeAll(oldDiary.getTrees().keySet());
-
-        updateTreeXp(oldDataSet, -5); // 제거된 tree에서 xp 제거
-        updateTreeXp(newDiary.getTrees().keySet(), 5);  // 추가된 tree에서 xp 추가
+        Map<String, DiaryData> diaries = new HashMap<String, DiaryData>();
+        diaries.put("new", newDiary);
+        diaries.put("old", oldDiary);
+        uploadDiarytoServer(diaries);
     }
 
     public void deleteDiaryFromServer(DiaryData diaryData){
@@ -100,9 +106,12 @@ public class DiaryHelper {
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
         String uid = FirebaseAuth.getInstance().getUid();
 
-        ref.child("Users").child(uid).child("diaries").child(diaryData.getDiaryID()).removeValue();
+        ref.child("Diaries").child(diaryData.getDiaryID()).removeValue();
         // tree xp update
         updateTreeXp(diaryData.getTrees().keySet(), -5);
+
+        // if image in storage ? delete image in firebase storage
+        if (!diaryData.getPicture().equals("")) FirebaseStorage.getInstance().getReferenceFromUrl(diaryData.getPicture()).delete();
     }
 
     private void updateTreeXp(Set<String> treeSet, int plus_xp){
@@ -139,5 +148,72 @@ public class DiaryHelper {
             }
         }
 
+    }
+
+    private void updateTreeXp(Map<String, Object> oldTrees, Map<String, Object> newTrees){
+        if (oldTrees.equals(newTrees)) return;
+        Set<String> oldDataSet = oldTrees.keySet();
+        Set<String> newDataSet = newTrees.keySet();
+        oldDataSet.removeAll(newTrees.keySet());
+        newDataSet.removeAll(oldTrees.keySet());
+
+        updateTreeXp(oldDataSet, -5); // 제거된 tree에서 xp 제거
+        updateTreeXp(newTrees.keySet(), 5);  // 추가된 tree에서 xp 추가
+    }
+
+    private void uploadDiarytoServer(Map<String,DiaryData> diaries){
+        ref = FirebaseDatabase.getInstance().getReference();
+        int flag = diaries.size();
+        String fileName = System.currentTimeMillis()+".jpg";
+
+        // if img is not updated or not selected, just update realtime db
+        if(diaries.get("new").getPicture().equals("")
+                || diaries.get("new").getPicture().contains("firebasestorage")){
+            ref = ref.child("Diaries").child(diaries.get("new").getDiaryID());
+            ref.updateChildren(diaries.get("new").getHash());
+
+            if (flag == 2) updateTreeXp(diaries.get("old").getTrees(), diaries.get("new").getTrees());
+            return;
+        }
+        Uri file = Uri.parse(diaries.get("new").getPicture());
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageReference = storage.getReference().child("Diaries/");
+
+        UploadTask uploadTask = storageReference.child(fileName).putFile(file);
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if(!task.isSuccessful()){
+                    throw  task.getException();
+                }
+                return storageReference.child(fileName).getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if(task.isSuccessful()){
+                    Uri downloadUri = task.getResult();
+                    diaries.get("new").setPicture(downloadUri.toString());
+
+                    if(flag == 1){ // insert
+                        Map<String, Object> childUpdate = new HashMap<String, Object>();
+                        childUpdate.put("Diaries/"+diaries.get("new").getDiaryID(), diaries.get("new").getHash());
+                        childUpdate.put("Users/"+diaries.get("new").getUid()+"/last_activity/",diaries.get("new").getDiaryID());
+                        ref.updateChildren(childUpdate);
+                        updateTreeXp(diaries.get("new").getTrees().keySet(), 5);
+                    }else if(flag == 2){ // update
+                        // delete previous image in firebase storage
+                        storage.getReferenceFromUrl(diaries.get("old").getPicture()).delete();
+
+                        ref = ref.child("Diaries").child(diaries.get("new").getDiaryID());
+                        ref.updateChildren(diaries.get("new").getHash());
+
+                        updateTreeXp(diaries.get("old").getTrees(), diaries.get("new").getTrees());
+                    }
+
+                    Log.d("DiaryHelper","사진 업로드 성공" );
+                }
+            }
+        });
     }
 }
