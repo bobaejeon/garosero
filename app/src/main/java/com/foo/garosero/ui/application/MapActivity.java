e4f5e7059e47a2c01090cf61cf883912c8d87fe0
bobaejeon <bbj5608@gmail.com>
2022-01-27 21:06:05 +0900

    minor refactoring

--- app/src/main/java/com/foo/garosero/ui/application/MapActivity.java
+++ app/src/main/java/com/foo/garosero/ui/application/MapActivity.java
@@ -3,7 +3,6 @@ package com.foo.garosero.ui.application;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
